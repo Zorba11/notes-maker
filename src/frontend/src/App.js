5660d8507933c37dd96ac69eec8dcb3b5540e77b
Zorba11 <zorbathegreek111@gmail.com>
2021-08-22 09:49:34 -0600

    react-router-dom, routing setup for home and create pages

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import Create from './pages/Create';
 
 function App() {
   return (
-    <div className="App">
-      Hello from react !
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
