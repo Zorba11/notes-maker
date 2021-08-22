fbeb39bbc9c14bcd48efe3275261efd7420521d3
Zorba11 <zorbathegreek111@gmail.com>
2021-08-22 16:15:12 -0600

    avatar circle for each note with different background colors based on category

--- src/frontend/src/components/NoteCard.js
+++ src/frontend/src/components/NoteCard.js
@@ -1,11 +1,47 @@
-import { Card, CardContent, CardHeader, Typography } from '@material-ui/core';
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import { orange, pink, purple, yellow } from '@material-ui/core/colors';
 import React from 'react';
 
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.category === 'work') {
+        return yellow[700];
+      }
+      if (note.category === 'money') {
+        return orange[500];
+      }
+      if (note.category === 'todos') {
+        return pink[500];
+      }
+
+      return purple[500];
+    },
+  },
+});
+
 const NoteCard = ({ note, handleDelete }) => {
+  const classes = useStyles(note);
+
   return (
     <div>
       <Card>
-        <CardHeader title={note.title} subheader={note.category} />
+        <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
+          title={note.title}
+          subheader={note.category}
+        />
         <CardContent>
           <Typography variant="body1" color="textSecondary">
             {note.details}
