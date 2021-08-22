4522dfb6e9f5eba8f19be13a864d889be6d76cb0
Zorba11 <zorbathegreek111@gmail.com>
2021-08-22 11:13:58 -0600

    create page, need to finish-handlesubmit, need to fix button position

--- src/frontend/src/pages/Create.js
+++ src/frontend/src/pages/Create.js
@@ -1,19 +1,122 @@
-import { Container, TextField, Typography } from '@material-ui/core';
-import React from 'react';
+import {
+  Button,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  makeStyles,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { KeyboardArrowRight } from '@material-ui/icons';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Create = () => (
-  <Container>
-    <Typography variant="h6" component="h2" color="textSecondary">
-      Creat a New Note
-    </Typography>
-    <form noValidate autoComplete="off">
-      <TextField
-        label="Note Title"
-        variant="outlined"
-        color="secondary"
-      ></TextField>
-      <TextField></TextField>
-    </form>
-  </Container>
-);
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block',
+  },
+});
+
+const Create = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [category, setCategory] = useState('work');
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title == '') setTitleError(true);
+
+    if (details == '') setDetailsError(true);
+
+    if (title && details) {
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ title, details, category }),
+      }).then(() => history.pushState('/'));
+    }
+  };
+
+  return (
+    <Container>
+      <Typography variant="h6" component="h2" color="textSecondary">
+        Creat a New Note
+      </Typography>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          className={classes.field}
+          onChange={(e) => setTitle(e.target.value)}
+          error={titleError}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+        ></TextField>
+        <TextField
+          className={classes.field}
+          onChange={(e) => setDetails(e.target.value)}
+          error={detailsError}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          multiline
+          rows={5}
+        ></TextField>
+
+        <FormControl classes={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminders"
+            />
+
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+
+            <FormControlLabel
+              value="financial"
+              control={<Radio />}
+              label="Financial"
+            />
+
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRight />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
 export default Create;
