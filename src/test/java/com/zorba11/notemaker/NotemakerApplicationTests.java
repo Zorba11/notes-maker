236a43dcdee60a781422aa0c937e46a980ecf912
Zorba11 <zorbathegreek111@gmail.com>
2021-08-27 19:37:21 -0600

    test fail

--- src/test/java/com/zorba11/notemaker/NotemakerApplicationTests.java
+++ src/test/java/com/zorba11/notemaker/NotemakerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.zorba11.notemaker;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class NotemakerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("oops again!");
 	}
 
 }
